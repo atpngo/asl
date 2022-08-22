fc4f379e60829b89ae84bbbc216ddabf15e4d937
Andy Ngo <andytpngo@g.ucla.edu>
2022-08-22 11:33:35 -0700

    experimenting with div vs button outline on mobile

--- frontend/src/components/Questions/OptionButton.js
+++ frontend/src/components/Questions/OptionButton.js
@@ -4,14 +4,14 @@ import { motion } from "framer-motion";
 function OptionButton({callback, children, correct})
 {
     return(
-        <motion.button 
+        <motion.div 
             whileHover={{scale: 1.1}} 
             whileTap={ correct ? { scale: 0.9 } : { rotate: [0, 10, -10, 10, -10, 10, -10, 0] }} 
             className="alphabet-outline-dark p-5 w-36" 
             onMouseDown={callback}
         >
             {children}
-        </motion.button>
+        </motion.div>
     )
 }
 
