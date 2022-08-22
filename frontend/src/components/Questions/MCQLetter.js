734701a953dd29757e81d0bae99f5315c2ece864
Andy Ngo <andytpngo@g.ucla.edu>
2022-08-22 10:56:40 -0700

    moved sounds to frontend

--- frontend/src/components/Questions/MCQLetter.js
+++ frontend/src/components/Questions/MCQLetter.js
@@ -1,48 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Sign from "../Sign";
-import { Howl } from 'howler';
 import BackendManager from "../../utils/BackendManager";
 
-function MCQLetter({prompt, answer, options, variant})
+
+function MCQLetter({answer, options, variant, callback })
 {
     // props:
     // answer: string
     // options: [string]
     // variant: 'identify' (identify sign) or 'select' (select sign)
 
-    const [sounds, setSounds] = useState({});
 
-    useEffect(() => {
-        setSounds({
-            'correct': new Howl({
-                src: `${BackendManager.model}/media/correct.mp3`,
-                html5: true,
-                volume: 0.05,
-                loop: false
-            }),
-            'incorrect': new Howl({   
-                src: `${BackendManager.model}/media/incorrect.mp3`,
-                html5: true,
-                volume: 0.05,
-                loop: false
-            })
-        });
-
-    }, []);
-    const handleOptionClick = (option) =>
-    {
-        // if correct, then play the gooood sound
-        if (option === answer)
-        {
-            console.log('Correct!');
-            sounds.correct.play();
-        }
-        else
-        {
-            console.log('Incorrect');
-            sounds.incorrect.play();
-        }
-    }
 
     return(
         <div className="flex flex-col items-center gap-12">
@@ -60,7 +28,7 @@ function MCQLetter({prompt, answer, options, variant})
                     
                     {options.map((option, index) => {
                         return(
-                            <button key={index} className="alphabet-outline-dark p-5 w-36 expand-btn" onClick={() => handleOptionClick(option)}>
+                            <button key={index} className="alphabet-outline-dark p-5 w-36 expand-btn" onClick={() => callback(option, answer)}>
                                 <p className="text-7xl p-0 m-0">{option}</p>
                             </button>
                         )
--- frontend/src/pages/Learn.js
+++ frontend/src/pages/Learn.js
@@ -1,11 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import MCQLetter from "../components/Questions/MCQLetter";
+import correctSound from "../media/correct.mp3";
+import wrongSound from "../media/incorrect.mp3";
+import { Howl } from 'howler';
+
 
 function Learn()
 {
+    let alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+    const [sounds, setSounds] = useState({});
+
+    useEffect(() => {
+        setSounds({
+            'correct': new Howl({
+                src: correctSound,
+                html5: true,
+                volume: 0.05,
+                loop: false
+            }),
+            'incorrect': new Howl({   
+                src: wrongSound,
+                html5: true,
+                volume: 0.05,
+                loop: false
+            })
+        });
+
+    }, []);
+
+    const optionSelected = (option, answer) =>
+    {
+        // if correct, then play the gooood sound
+        if (option === answer)
+        {
+            console.log('Correct!');
+            sounds.correct.play();
+
+            // next question
+            
+        }
+        else
+        {
+            console.log('Incorrect');
+            sounds.incorrect.play();
+        }
+    }
     return(
         <div>
-            <MCQLetter answer="S" options={['Q', 'M', 'S', 'A']} variant="identify"/>
+            <MCQLetter answer="S" options={['Q', 'M', 'S', 'A']} variant="identify" callback={optionSelected}/>
         </div>
     )
 }
